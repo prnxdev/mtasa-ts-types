9acd94d2cfa6b8abe590f7b75537f2de0428e06b
Paweł Miczka <piorunx@gmail.com>
2021-12-05 00:00:53 +0100

    0.1.14

--- server.d.ts
+++ server.d.ts
@@ -2,4 +2,5 @@
 /// <reference path='./types/server/functions/acl-functions.d.ts' />
 /// <reference path='./types/server/functions/admin-functions.d.ts' />
 /// <reference path='./types/server/functions/event-functions.d.ts' />
+/// <reference path='./types/server/functions/utility-functions.d.ts' />
 /// <reference path='./types/server/variables.d.ts' />
