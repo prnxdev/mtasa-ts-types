ce24b3a01a32b2ac4135399023e0cbf8e5906391
Paweł Miczka <piorunx@gmail.com>
2021-12-06 16:36:26 +0100

    setTimer function

--- types/server/functions/utility-functions.d.ts
+++ types/server/functions/utility-functions.d.ts
@@ -1,3 +1,5 @@
 /** @noSelfInFile */
 
 declare function iprint(...args: unknown[]): void;
+
+declare function setTimer(callback: Function, interval: number, timesToExecute: number, ...args: unknown[]): void; //TODO: add proper return
