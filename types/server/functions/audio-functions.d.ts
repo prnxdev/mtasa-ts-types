7871a326292bc50851130b315dd7b1038b86e527
Piorun <piorunx@gmail.com>
2022-04-20 08:45:25 +0000

    added server-side audio functions

---
+++ types/server/functions/audio-functions.d.ts
@@ -0,0 +1,7 @@
+/// <reference path='../classes/Player.d.ts' />
+
+/** @noSelfInFile */
+
+// bool playSoundFrontEnd ( player thePlayer, int sound )   
+
+declare function playSoundFrontEnd(player: Player, sound: number): boolean;
