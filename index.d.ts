3e84b83f64a10224c7b07e82d6951f0428c7c244
prnxdev <piorunx@gmail.com>
2022-04-25 12:46:35 +0000

    removed `index.d.ts`

--- index.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="./node_modules/typescript-to-lua/language-extensions/index.d.ts" />
--- server.d.ts
+++ server.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="./node_modules/typescript-to-lua/language-extensions/index.d.ts" />
+
 /// <reference path='./types/server/functions/account-functions.d.ts' />
 /// <reference path='./types/server/functions/acl-functions.d.ts' />
 /// <reference path='./types/server/functions/admin-functions.d.ts' />
