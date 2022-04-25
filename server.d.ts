d30513230ddb3529fb28d0bae232cc18964f397a
prnxdev <piorunx@gmail.com>
2022-04-25 13:19:54 +0000

    added classes into server declarations file

--- server.d.ts
+++ server.d.ts
@@ -1,3 +1,13 @@
+/// <reference path='./types/server/classes/Account.d.ts' />
+/// <reference path='./types/server/classes/ACL.d.ts' />
+/// <reference path='./types/server/classes/ACLGroup.d.ts' />
+/// <reference path='./types/server/classes/Ban.d.ts' />
+/// <reference path='./types/server/classes/Blip.d.ts' />
+/// <reference path='./types/server/classes/ColShape.d.ts' />
+/// <reference path='./types/server/classes/Marker.d.ts' />
+/// <reference path='./types/server/classes/MTAElement.d.ts' />
+/// <reference path='./types/server/classes/Player.d.ts' />
+
 /// <reference path='./types/server/functions/account-functions.d.ts' />
 /// <reference path='./types/server/functions/acl-functions.d.ts' />
 /// <reference path='./types/server/functions/admin-functions.d.ts' />
